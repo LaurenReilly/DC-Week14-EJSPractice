9bc3cfa8bf2a2f83af78ad97bf5fe22c3866e807
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-01-03 18:08:23 -0500

    site loops through object and uses partials to build page based off template

--- server.js
+++ server.js
@@ -4,36 +4,81 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//setting our view engine as ejs and telling it where the views will be located
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
-var animals = [
-{
-    "name":"Minerva",
-    "description":"My first born",
-    "imgURL":"app/images/minerva.jpg"
-},
-{
-    "name":"Luna",
-    "description":"The cuddliest loving cat",
-    "imgURL":"app/images/luna.jpg"
-},
-{
-    "name":"Frances",
-    "description":"Found her on the streets",
-    "imgURL":"app/images/frances.jpg"
-},
-{
-    "name":"Boethiah",
-    "description":"Surprise baby",
-    "imgURL":"app/images/boethiah.jpg"
-},
-{
-    "name":"Selwyn",
-    "description":"Rescued friend",
-    "imgURL":"app/images/selwyn.jpg"
-}];
+// var animals = {animals: [
+// {
+//     name:"Minerva",
+//     description:"My first born",
+//     imgURL:"app/images/minerva.jpg"
+// },
+// {
+//     name:"Luna",
+//     description:"The cuddliest loving cat",
+//     imgURL:"app/images/luna.jpg"
+// },
+// {
+//     name:"Frances",
+//     description:"Found her on the streets",
+//     imgURL:"app/images/frances.jpg"
+// },
+// {
+//     name:"Boethiah",
+//     description:"Surprise baby",
+//     imgURL:"app/images/boethiah.jpg"
+// },
+// {
+//     name:"Selwyn",
+//     description:"Rescued friend",
+//     imgURL:"app/images/selwyn.jpg"
+// }]};
 
+// app.get('/pets', function(req, res){
+//     res.render('pets', {
+//         title: "Lauren's Page",
+//         tagline: "I hope I can become pro!"
+//     });
+// });
+
+//telling server where the files are located that it can use
+app.use(express.static(__dirname + '/app'));
+
+app.get('/pets', function(req, res){
+    res.render('pets', {animals: [
+        {
+            name:"Minerva",
+            description:"My first born",
+            imgURL:"images/minerva.jpg"
+        },
+        {
+            name:"Luna",
+            description:"The cuddliest loving cat",
+            imgURL:"images/luna.jpg"
+        },
+        {
+            name:"Frances",
+            description:"Found her on the streets",
+            imgURL:"images/frances.jpg"
+        },
+        {
+            name:"Boethiah",
+            description:"Surprise baby",
+            imgURL:"images/boethiah.jpg"
+        },
+        {
+            name:"Selwyn",
+            description:"Rescued friend",
+            imgURL:"images/selwyn.jpg"
+        }]}
+    );
+});
+
+//server listening for requests
 app.listen(3000, function(){
     console.log("Listening on port 3000");
 });
